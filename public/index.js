03d6d4ca3f6ed6d362c31cf05ea368e93733fe73
Geraldo Junior Dias Melo <geraldo.melo@novaandradina.org>
2019-06-27 16:20:06 -0300

    adicionando arquivos

--- public/index.js
+++ public/index.js
@@ -1,52 +1,92 @@
-function calcular() {
-    var inpu = document.getElementById('respp').value;//a variavel inpu recebe o valor coletado ("número de chaves")
-    var input2 = document.getElementById('inputt').value.toUpperCase();// a variavel inputt recebe o valor coletado ("letra(s)") e as tranforma em caixa alta 
-    var result = "";// inicializa a variavel
+function cifrar() {
+    var palavra = document.getElementById("inputt").value.toLowerCase();
+    var chave = document.getElementById("respp").value.toLowerCase();
+    var alfabeto = "abcdefghijklmnopqrstuvwxyz";
+    var numsim = '1234567890!@#$%¨&*()_+,./|';
+    var resultado = "";
+    var j = 0;
 
-    if(inpu <1){ // se valor de chaves digitado for menor que 1 , valor de chaves automaticamente vira 1
-        inpu=1;
-    }else if(inpu >25){ // se valor de chaves digitado for maior que 25 , valor de chaves digitado vira automaticamente  25
-        inpu=25;
+    for (var i = 0; i < palavra.length; i++) {
+
+        if (j >= chave.length) {
+            j = 0;
+        }
+
+        if (palavra.charAt(i) === " ") {
+            resultado = resultado + " ";
+            ++i;
+        }
+
+        for (let k = 0; k < numsim.length; k++) {
+            if (palavra.charAt(i) === numsim.charAt(k)) {
+                resultado = resultado + numsim.charAt(k)
+                ++i;
+            }
+        }
+
+        if (i < palavra.length) {
+            var posicaochave = alfabeto.indexOf(chave.charAt(j)); //pega posição das letras da chave
+            var posicaoletra = alfabeto.indexOf(palavra.charAt(i)); //pega posição das letras da palavra
+            var novaChave = posicaochave + posicaoletra; //soma os dois de cima para fazer a criptografia
+
+            if (novaChave >= 25) {
+                novaChave = novaChave - 26;
+            }
+
+            if (novaChave < 0) {
+                novaChave = novaChave + 26;
+            }
+
+            resultado = resultado + alfabeto.charAt(novaChave);
+
+            j++;
+        }
     }
-    for(var i = 0; i<input2.length; i++){ 
-       var posicaoDaLetraNoAlfabeto = input2.charCodeAt(i)-64; // a variavel recebe o as letras e a tranforma em id e a joga na tabela ascii e tira -64 que e quando começa o alfabeto em caixa alta da tabela ascii
-      
-       var letraComDeslocamento =  parseInt(posicaoDaLetraNoAlfabeto) + parseInt(inpu);; // o id da letra ou letras digitadas somado com o numero de chaves
-       
-       if(letraComDeslocamento >26){// se maior que 26 a soma acima , a soma e dividida por 26 e o resto e contando, como se passase do Z e voltasse pro A
-        var letraComDeslocamento = letraComDeslocamento % 26;
-       }
-        result += String.fromCharCode(parseInt(letraComDeslocamento)+64); // faz o id virar numero novamente 
-    
-    }
-    document.getElementById("output").innerHTML = result; // mostra o resultado 
+    document.getElementById("output").innerHTML = resultado;
 }
 
-function desfazer() {
-    var inpu = document.getElementById('respp').value;
-    var input2 = document.getElementById('inputt').value.toUpperCase();
-    var result = "";
+function decifrar() {
+    var palavra = document.getElementById("inputt").value.toLowerCase();
+    var chave = document.getElementById("respp").value.toLowerCase();
+    var alfabeto = "abcdefghijklmnopqrstuvwxyz";
+    var numsim = '1234567890!@#$%¨&*()_+,./|';
+    var resultado = "";
+    var j = 0;
+    for (var i = 0; i < palavra.length; i++) {
 
-    if(inpu <1){
-        inpu=1;
-    }else if(inpu >25){
-        inpu=25;
-    }
-    
-    for(var i = 0; i<input2.length; i++){ 
-     
-     
-     
-       var posicaoDaLetraNoAlfabeto = input2.charCodeAt(i)-65; 
-
-      
-       var letraComDeslocamento = parseInt(posicaoDaLetraNoAlfabeto) - parseInt(inpu);// o id da letra ou letras digitadas somado com o numero de chaves subtraido com o numero de chaves
-       
-       if(letraComDeslocamento <0){// se menor que 0 a subtração acima  , subtração recebee 26 mais o numero de chaves ,como se passase do A e voltasse pro Z
-        var letraComDeslocamento = 26 + letraComDeslocamento ;
+        if (j >= chave.length) {
+            j = 0;
         }
-        result += String.fromCharCode(parseInt(letraComDeslocamento)+65); 
-    
+
+        if (palavra.charAt(i) === " ") {
+            resultado = resultado + " ";
+            ++i;
+        }
+
+        for (let k = 0; k < numsim.length; k++) {
+            if (palavra.charAt(i) === numsim.charAt(k)) {
+                resultado = resultado + numsim.charAt(k)
+                ++i;
+            }
+        }
+
+        if (i < palavra.length) {
+            var posicaochave = alfabeto.indexOf(chave.charAt(j));
+            var posicaoletra = alfabeto.indexOf(palavra.charAt(i));
+            var novaChave = posicaoletra - posicaochave;
+
+            if (novaChave >= 25) {
+                novaChave = novaChave - 26;
+            }
+
+            if (novaChave < 0) {
+                novaChave = novaChave + 26;
+            }
+
+            resultado = resultado + alfabeto.charAt(novaChave);
+            j++;
+        }
     }
-    document.getElementById("output").innerHTML = result;
+
+    document.getElementById("output").innerHTML = resultado;
 }
